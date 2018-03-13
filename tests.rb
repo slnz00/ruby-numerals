ef172b30162c5fd30615b339898d12b7c4ae384c
Balint Bakcsa <balintbakcsa@gmail.com>
2018-03-13 22:13:01 +0100

    Tests for predefined examples

--- tests.rb
+++ tests.rb
@@ -9,4 +9,26 @@ class TestEnglishWordsFromNumbers < Minitest::Test
 
   def teardown; end
 
+  def test_examples
+    assert_equal(
+      'seven',
+      @numerals.convert_to_english_words(7))
+    assert_equal(
+      'Answer to the Ultimate Question of Life, the Universe, and Everything',
+      @numerals.convert_to_english_words(42)
+    )
+    assert_equal(
+      'two thousand and one',
+      @numerals.convert_to_english_words(2001)
+    )
+    assert_equal(
+      'nineteen hundred and ninety-nine',
+      @numerals.convert_to_english_words(1999)
+    )
+    assert_equal(
+      'seventeen thousand nine hundred and ninety-nine',
+      @numerals.convert_to_english_words(17_999)
+    )
+  end
+
 end
