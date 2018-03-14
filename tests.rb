7ec6d0eafa6694c4ac21cc9bd7d3f09a2a53b7d5
Balint Bakcsa <balintbakcsa@gmail.com>
2018-03-14 02:09:56 +0100

    Test for 0 input value

--- tests.rb
+++ tests.rb
@@ -16,6 +16,10 @@ class TestEnglishWordsFromNumbers < Minitest::Test
 
   def test_examples
     assert_equal(
+        'zero',
+        @numerals.convert_to_english_words(0)
+    )
+    assert_equal(
       'seven',
       @numerals.convert_to_english_words(7)
     )
